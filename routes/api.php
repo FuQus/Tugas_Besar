294c84c80d23f20035395f877c967d672aeb6931
FuQus <mubdilazuard1105@gmail.com>
2024-12-13 14:43:47 +0800

    web.php dirubah

--- routes/api.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace App\Http\Controllers\Auth;
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Auth\RegisterController;
-
-Route::post('/register', [RegisterController::class, 'register']);
--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,7 @@ use App\Http\Controllers\Auth\RegisterController;
 Route::post('/register', [RegisterController::class, 'register']);
 
 Route::get('/', function () { return view('welcome');});
+
+Route::get('/register', function () {
+    return view('auth.register'); // Menampilkan form registrasi
+});
